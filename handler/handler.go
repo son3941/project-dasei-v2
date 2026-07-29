b161cebe1d729bdc5c76b94eb0e616bc1b031d93
son3941 <manchstergiggs@yahoo.co.jp>
2026-07-29 23:19:31 +0900

    メンバー記憶

--- handler/handler.go
+++ handler/handler.go
@@ -59,7 +59,13 @@ func (h *Handler) Handle(ctx context.Context, ev *modelv1.Event) error {
 		h.logger.Info("event", slog.Any("event", ev))
 		h.logger.Info("post data", slog.Any("post", post.GetPost()))
 		text := post.GetPost().GetText()
+		displayName := ""
+		if post.GetIssuer() != nil {
+			displayName = post.GetIssuer().GetDisplayName()
+			rememberMember(displayName)
+		}
 		isMention := strings.Contains(text, "@dasei")
+
 		reply := GenerateReply(
 			text,
 			isMention,
