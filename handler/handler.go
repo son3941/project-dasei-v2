562c625a3ff4fb0a7280d72b0be499961698352e
son3941 <manchstergiggs@yahoo.co.jp>
2026-07-31 09:38:40 +0900

    更新３

--- handler/handler.go
+++ handler/handler.go
@@ -323,6 +323,22 @@ func createMutter(text string) string {
 		"えーとえーと",
 	}
 
+	if rand.Intn(100) < 80 && len(memories) > 0 {
+		memoryMu.RLock()
+		values := make([]string, 0, len(memories))
+		for _, m := range memories {
+			values = append(values, m.Value)
+		}
+		memoryMu.RUnlock()
+
+		if len(values) > 0 {
+			return randomReply(
+				values[rand.Intn(len(values))],
+				"たぶん"+values[rand.Intn(len(values))],
+				values[rand.Intn(len(values))]+"だった気がする",
+			)
+		}
+	}
 	return mutters[rand.Intn(len(mutters))]
 }
 func remember(text string) {
