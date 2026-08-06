f1b9cf74c342802598303fb2c84f8c12a2d2445e
son3941 <manchstergiggs@yahoo.co.jp>
2026-08-06 11:55:21 +0900

    q

--- handler/handler.go
+++ handler/handler.go
@@ -199,11 +199,6 @@ func (h *Handler) Handle(ctx context.Context, ev *modelv1.Event) error {
 			return nil
 		}
 
-		// 「だせい」だけの呼びかけは学習しない
-		if strings.TrimSpace(text) == "だせい" {
-			return nil
-		}
-
 		rememberKnowledge(text)
 
 		if err := SavePost(text); err != nil {
