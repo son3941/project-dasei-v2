88b93bf55771c16d6f8c72d72a0978303fe3b4eb
son3941 <manchstergiggs@yahoo.co.jp>
2026-08-10 19:05:17 +0900

    de

--- handler/handler.go
+++ handler/handler.go
@@ -1123,10 +1123,6 @@ func (h *Handler) handleChatMessage(ctx context.Context, ev *modelv1.ChatMessage
 
 	isMention := strings.Contains(userText, "@dasei")
 
-	if !isMention && fixedReply(userText) == "" && rand.Intn(100) >= 50 {
-		return nil
-	}
-
 	reply := GenerateReply(userText, isMention)
 
 	authCtx, err := h.authenticator.AuthorizedContext(ctx)
