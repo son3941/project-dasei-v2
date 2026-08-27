86aa0baef99ada5c586e8368ed8b2872a34aac65
son3941 <manchstergiggss@yahoo.co.jp>
2026-08-27 11:57:53 +0900

    debug post type

--- handler/handler.go
+++ handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"html"
 	"io"
 	"log"
@@ -482,6 +483,9 @@ func (h *Handler) Handle(ctx context.Context, ev *modelv1.Event) error {
 		h.logger.Info(
 			"post object",
 			slog.Any("post", post.GetPost()),
+		)
+		slog.Info("post type",
+			slog.String("type", fmt.Sprintf("%T", post.GetPost())),
 		)
 		communityID = post.GetPost().GetCommunityId()
 		h.communityID = communityID
